cef575e1be97e9fee81289e999b79908a9f74951
Mikhail Pokhikhilov <thedilletante@gmail.com>
2019-09-25 23:13:25 +0300

    Some cosmetic changes

--- src/msg/id.rs
+++ src/msg/id.rs
@@ -1,76 +1,97 @@
-fn generate(input: &[u8], output: &mut [char], lookup_table: &'static [char]) -> Result<usize, ()> {
-  let mut i: usize = 0;
 
-  if !input.is_empty() {
-    let mut num : usize = 0;
-    for x in input {
-      num *= 256;
-      num += *x as usize;
+type Byte = u8;
+type Binary = [Byte];
+type Result = std::result::Result<usize, ()>;
 
-      while num > lookup_table.len() {
-        if i >= output.len() {
-          return Err(())
-        }
-        output[i] = lookup_table[num % lookup_table.len()];
-        num /= lookup_table.len();
-        i += 1;
-      }
-    }
-    if num > 0 {
-      if i >= output.len() {
-        return Err(())
-      }
-      output[i] = lookup_table[num % lookup_table.len()];
-      i += 1;
+pub fn token(input: &Binary, output: &mut Binary) -> Result  {
+  Generator { input, output, lookup: TOKEN_LOOKUP_TABLE }.generate()
+}
+
+pub fn word(input: &Binary, output: &mut Binary) -> Result {
+  Generator { input, output, lookup: WORD_LOOKUP_TABLE }.generate()
+}
+
+
+struct Generator <'a, 'b> {
+  input: &'a Binary,
+  output: &'b mut Binary,
+  lookup: &'static Binary
+}
+
+impl <'a, 'b> Generator <'a, 'b> {
+
+  fn put(&mut self, i: &mut usize, acc: &mut usize) -> std::result::Result<(), ()> {
+    if *i >= self.output.len() {
+      Err(())
+    } else {
+      self.output[*i] = self.lookup[*acc % self.lookup.len()] as u8;
+      *acc /= self.lookup.len();
+      *i += 1;
+      Ok(())
     }
   }
 
-  Ok(i)
+  fn generate(&mut self) -> Result {
+    let mut i = 0;
+
+    if !self.input.is_empty() {
+      let mut acc : usize = 0;
+      for x in self.input {
+        acc *= 256;
+        acc += *x as usize;
+
+        while acc > self.lookup.len() {
+          self.put(&mut i, &mut acc)?
+        }
+      }
+
+      while acc > 0 {
+        self.put(&mut i, &mut acc)?
+      }
+    }
+
+    Ok(i)
+  }
+
 }
 
-static TOKEN_LOOKUP_TABLE: &'static [char] = &[
+static TOKEN_LOOKUP_TABLE: &'static Binary = &[
   // other symbols
-  '-', '.', '!', '*', '_', '+', '`', '\'', '~',
+  '-' as u8, '.' as u8, '!' as u8, '*' as u8, '_' as u8, '+' as u8, '`' as u8, '\'' as u8, '~' as u8,
   // numbers
-  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  '0' as u8, '1' as u8, '2' as u8, '3' as u8, '4' as u8, '5' as u8, '6' as u8, '7' as u8, '8' as u8, '9' as u8,
   // lower case letters
-  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-  'u', 'v', 'w', 'x', 'y', 'z',
+  'a' as u8, 'b' as u8, 'c' as u8, 'd' as u8, 'e' as u8, 'f' as u8, 'g' as u8, 'h' as u8, 'i' as u8, 'j' as u8,
+  'k' as u8, 'l' as u8, 'm' as u8, 'n' as u8, 'o' as u8, 'p' as u8, 'q' as u8, 'r' as u8, 's' as u8, 't' as u8,
+  'u' as u8, 'v' as u8, 'w' as u8, 'x' as u8, 'y' as u8, 'z' as u8,
   // upper case letters
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-  'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-  'U', 'V', 'W', 'X', 'Y', 'Z'
+  'A' as u8, 'B' as u8, 'C' as u8, 'D' as u8, 'E' as u8, 'F' as u8, 'G' as u8, 'H' as u8, 'I' as u8, 'J' as u8,
+  'K' as u8, 'L' as u8, 'M' as u8, 'N' as u8, 'O' as u8, 'P' as u8, 'Q' as u8, 'R' as u8, 'S' as u8, 'T' as u8,
+  'U' as u8, 'V' as u8, 'W' as u8, 'X' as u8, 'Y' as u8, 'Z' as u8
 ];
 
-pub fn token(input: &[u8], output: &mut [char]) -> Result<usize, ()>  {
-  generate(input, output, TOKEN_LOOKUP_TABLE)
-}
-
-static WORD_LOOKUP_TABLE: &'static [char] = &[
+static WORD_LOOKUP_TABLE: &'static Binary = &[
   // other symbols
-  '-', '.', '!', '*', '_', '+', '`', '\'', '~', '(', ')',
-  '<', '>', ':', '\\', '"', '/', '[', ']', '?', '{', '}',
+  '-' as u8, '.' as u8, '!' as u8, '*' as u8, '_' as u8, '+' as u8, '`' as u8, '\'' as u8, '~' as u8, '(' as u8, ')' as u8,
+  '<' as u8, '>' as u8, ':' as u8, '\\' as u8, '"' as u8, '/' as u8, '[' as u8, ']' as u8, '?' as u8, '{' as u8, '}' as u8,
   // numbers
-  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  '0' as u8, '1' as u8, '2' as u8, '3' as u8, '4' as u8, '5' as u8, '6' as u8, '7' as u8, '8' as u8, '9' as u8,
   // lower case letters
-  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-  'u', 'v', 'w', 'x', 'y', 'z',
+  'a' as u8, 'b' as u8, 'c' as u8, 'd' as u8, 'e' as u8, 'f' as u8, 'g' as u8, 'h' as u8, 'i' as u8, 'j' as u8,
+  'k' as u8, 'l' as u8, 'm' as u8, 'n' as u8, 'o' as u8, 'p' as u8, 'q' as u8, 'r' as u8, 's' as u8, 't' as u8,
+  'u' as u8, 'v' as u8, 'w' as u8, 'x' as u8, 'y' as u8, 'z' as u8,
   // upper case letters
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-  'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-  'U', 'V', 'W', 'X', 'Y', 'Z'
+  'A' as u8, 'B' as u8, 'C' as u8, 'D' as u8, 'E' as u8, 'F' as u8, 'G' as u8, 'H' as u8, 'I' as u8, 'J' as u8,
+  'K' as u8, 'L' as u8, 'M' as u8, 'N' as u8, 'O' as u8, 'P' as u8, 'Q' as u8, 'R' as u8, 'S' as u8, 'T' as u8,
+  'U' as u8, 'V' as u8, 'W' as u8, 'X' as u8, 'Y' as u8, 'Z' as u8
 ];
 
-pub fn word(input: &[u8], output: &mut [char]) -> Result<usize, ()> {
-  generate(input, output, WORD_LOOKUP_TABLE)
-}
 
 #[cfg(test)]
 mod tests {
   extern crate rand;
 
+  use rand::Rng;
   use super::*;
 
   #[test]
@@ -79,34 +100,38 @@ mod tests {
     assert_eq!(word(&[], &mut []), Ok(0));
   }
 
-  fn is_token_char(ch: &char) -> bool {
+  fn is_token_char(ch: &u8) -> bool {
     TOKEN_LOOKUP_TABLE.contains(&ch)
   }
 
-  fn is_word_char(ch: &char) -> bool {
+  fn is_word_char(ch: &u8) -> bool {
     WORD_LOOKUP_TABLE.contains(ch)
   }
 
   #[test]
   fn token_test() {
+    let mut rnd = rand::thread_rng();
+    let mut output = vec![0; 100];
     for _ in 0..100 {
       let input: [u8; 30] = rand::random();
-      let mut output = vec!['0'; 100];
-      let size = token(&input, &mut output);
+      let size = token(&input[..rnd.gen_range(1, 30)], &mut output);
       assert!(size.is_ok());
       assert!(size.unwrap() > 0);
+      println!("token is {}", String::from_utf8(output[0..size.unwrap()].to_vec()).unwrap());
       output[0..size.unwrap()].into_iter().for_each(|ch| assert!(is_token_char(ch)));
     }
   }
 
   #[test]
   fn word_test() {
+    let mut rnd = rand::thread_rng();
+    let mut output = vec![0; 100];
     for _ in 0..100 {
       let input: [u8; 30] = rand::random();
-      let mut output = vec!['0'; 100];
-      let size = word(&input, &mut output);
+      let size = word(&input[..rnd.gen_range(1, 30)], &mut output);
       assert!(size.is_ok());
       assert!(size.unwrap() > 0);
+      println!("word is {}", String::from_utf8(output[0..size.unwrap()].to_vec()).unwrap());
       output[0..size.unwrap()].into_iter().for_each(|ch| assert!(is_word_char(ch)));
     }
   }
@@ -116,7 +141,7 @@ mod tests {
     assert_eq!(token(&[1], &mut[]), Err(()));
     assert_eq!(word(&[4, 6], &mut[]), Err(()));
 
-    let mut output = vec!['0'; 2];
+    let mut output = vec![0; 2];
     assert_eq!(token(&[1, 3, 4], &mut output), Err(()));
     assert_eq!(word(&[223, 45], &mut output), Err(()));
   }
