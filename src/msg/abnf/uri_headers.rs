7047ac316592150b429f114b9a53ecdc8479d29b
Mikhail Pokhikhilov <thedilletante@gmail.com>
2019-10-04 11:44:33 +0300

    Flatten out parameter for headers parsing

--- src/msg/abnf/uri_headers.rs
+++ src/msg/abnf/uri_headers.rs
@@ -2,8 +2,7 @@ use super::map_byte;
 use super::digit::single_hex;
 use super::ch::alphanum;
 use crate::msg::Binary;
-use nom::{IResult, Needed};
-use nom::Err::{Incomplete};
+use nom::IResult;
 
 // hnv-unreserved  =  "[" / "]" / "/" / "?" / ":" / "+" / "$"
 named!(#[inline],
@@ -69,30 +68,41 @@ named!(#[inline],
   tuple!(hname, preceded!(byte!(b'='), hvalue))
 );
 
+#[derive(Debug, Eq, PartialEq)]
+pub enum ParseError {
+  NotEnoughOutSpace {
+    written: usize
+  }
+}
+
 // headers         =  "?" header *( "&" header )
 #[inline]
-pub fn headers<'a, 'b>(input: &'a Binary, out: &'b mut [(&'a Binary, &'a Binary)]) -> IResult<&'a Binary, usize> {
+pub fn headers<'a, 'b>(input: &'a Binary, out: &'b mut [&'a Binary])
+  -> Result<(&'a Binary, usize), ParseError> {
+
   let mut i = 0usize;
   let mut rest = input;
   if let Ok((r, _)) = byte!(rest, b'?') {
     rest = r;
-    if let Ok((r, h)) = header(rest) {
-      if i >= out.len() {
-        return Err(Incomplete(Needed::Size(1)));
+    if let Ok((r, (hname, hvalue))) = header(rest) {
+      if i + 1 >= out.len() {
+        return Err(ParseError::NotEnoughOutSpace { written: i });
       }
 
       rest = r;
-      out[i] = h;
-      i += 1;
+      out[i] = hname;
+      out[i + 1] = hvalue;
+      i += 2;
 
-      while let Ok((r, h)) = preceded!(rest, byte!(b'&'), header) {
-        if i >= out.len() {
-          return Err(Incomplete(Needed::Size(1)));
+      while let Ok((r, (hname, hvalue))) = preceded!(rest, byte!(b'&'), header) {
+        if i + 1 >= out.len() {
+          return Err(ParseError::NotEnoughOutSpace { written: i });
         }
 
         rest = r;
-        out[i] = h;
-        i += 1;
+        out[i] = hname;
+        out[i + 1] = hvalue;
+        i += 2;
       }
     }
   }
@@ -104,19 +114,23 @@ pub fn headers<'a, 'b>(input: &'a Binary, out: &'b mut [(&'a Binary, &'a Binary)
 #[cfg(test)]
 mod tests {
   use super::headers;
-  use crate::msg::Binary;
+  use crate::msg::abnf::uri_headers::ParseError;
 
   #[test]
   fn headers_test() {
-    let mut hdrs: [(&Binary, &Binary); 10] = [(&[], &[]); 10];
-    assert_eq!(headers("?a=b&c=&%ff=_/.?".as_bytes(), &mut hdrs), Ok(("".as_bytes(), 3)));
+    let mut hdrs = ["".as_bytes(); 10];
+    assert_eq!(headers("?a=b&cdf=&[%ff]=_/.?".as_bytes(), &mut hdrs), Ok(("".as_bytes(), 6)));
 
-    assert_eq!(hdrs[0].0, "a".as_bytes());
-    assert_eq!(hdrs[0].1, "b".as_bytes());
-    assert_eq!(hdrs[1].0, "c".as_bytes());
-    assert_eq!(hdrs[1].1, "".as_bytes());
-    assert_eq!(hdrs[2].0, "%ff".as_bytes());
-    assert_eq!(hdrs[2].1, "_/.?".as_bytes());
+    assert_eq!(hdrs[0], "a".as_bytes());
+    assert_eq!(hdrs[1], "b".as_bytes());
+    assert_eq!(hdrs[2], "cdf".as_bytes());
+    assert_eq!(hdrs[3], "".as_bytes());
+    assert_eq!(hdrs[4], "[%ff]".as_bytes());
+    assert_eq!(hdrs[5], "_/.?".as_bytes());
+
+    assert_eq!(headers("?a=b".as_bytes(), &mut []), Err(ParseError::NotEnoughOutSpace { written: 0 }));
+    assert_eq!(headers("?a=b&c=d".as_bytes(), &mut hdrs[..1]), Err(ParseError::NotEnoughOutSpace { written: 0 }));
+    assert_eq!(headers("?a=b&c=d".as_bytes(), &mut hdrs[..2]), Err(ParseError::NotEnoughOutSpace { written: 2 }));
   }
 
 
